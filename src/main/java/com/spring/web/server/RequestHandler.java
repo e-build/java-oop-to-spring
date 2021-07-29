5e9f13a0f9d40cad929842481605a13c211483ba
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-07-29 16:07:47 +0900

    - 서버 소켓 및 포트 초기 코드 복사 (JWP Next Step)

---
+++ src/main/java/com/spring/web/server/RequestHandler.java
@@ -0,0 +1,63 @@
+package com.spring.web.server;
+
+import com.spring.http.HttpRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+
+public class RequestHandler implements Runnable  {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+    private Socket connection;
+
+    public RequestHandler(Socket connectionSocket) {
+        this.connection = connectionSocket;
+    }
+
+    public void run() {
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
+
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
+            StringBuilder httpStringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            String firstLine;
+            while ( StringUtils.isNotBlank(line = bufferedReader.readLine()) )
+                httpStringBuilder.append(line).append("\n");
+            HttpRequest httpRequest = HttpRequest.parseFromHttpString(httpStringBuilder.toString());
+
+            DataOutputStream dos = new DataOutputStream(out);
+            byte[] body = "Hello World".getBytes();
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
