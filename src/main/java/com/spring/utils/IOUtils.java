2eed72933a7c2995db32581ab05480e485fe6135
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-07-31 11:58:43 +0900

    입출력 유틸클래스 추가

---
+++ src/main/java/com/spring/utils/IOUtils.java
@@ -0,0 +1,13 @@
+package com.spring.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class IOUtils {
+    public static String readData(BufferedReader br, int length) throws IOException {
+        char[] body = new char[length];
+        br.read(body, 0, length);
+        return String.copyValueOf(body);
+    }
+
+}
