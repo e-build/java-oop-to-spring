dc5b980481a018966a8cf808c18c6f8be650ccc3
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-07-31 12:59:55 +0900

    웹 리소스 루트 디렉토리 추가 PREFIX, SUFFIX 추가

--- src/main/java/com/spring/utils/WebAppUtils.java
+++ src/main/java/com/spring/utils/WebAppUtils.java
@@ -2,5 +2,8 @@ package com.spring.utils;
 
 public class WebAppUtils {
     // TODO: 프로젝트 루트 디렉토리를 파악하는 자바 네이티브 로직으로 변경 필요
-    public static String WEBAPP_PATH = "/Users/donggeollee/IdeaProjects/ebuild-github/java-oop-to-spring/webapp/views";
+    public static String WEBAPP_ROOT_PATH = "/Users/donggeollee/IdeaProjects/ebuild-github/java-oop-to-spring/webapp";
+    public static String PREFIX = "/views";
+    public static String SUFFIX = ".html";
+
 }
