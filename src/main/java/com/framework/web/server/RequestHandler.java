6c024bb40ebdd20ac39760afcdeab0f7c68432b0
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-07-31 19:18:54 +0900

    HttpResponse 추가에 따른 로직 수정

---
+++ src/main/java/com/framework/web/server/RequestHandler.java
@@ -0,0 +1,42 @@
+package com.framework.web.server;
+
+import com.framework.http.*;
+import com.framework.utils.WebAppUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+
+public class RequestHandler implements Runnable  {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    private Socket connection;
+
+    public RequestHandler(Socket connectionSocket) {
+        this.connection = connectionSocket;
+    }
+
+    public void run() {
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            HttpRequest httpRequest = new HttpRequest(in);
+            HttpResponse httpResponse = new HttpResponse(out);
+            log.info("[HTTP REQUEST] : {} {}", httpRequest.getMethod(), httpRequest.getPath());
+
+            Controller controller = RequestMapping.getController(HandlerKey.of(HttpConstants.Method.GET, httpRequest.getPath()));
+            httpResponse.addHeader(HttpConstants.Header.CONTENT_TYPE, httpRequest.getSimpleAccept() + "; charset=UTF-8");
+            if ( controller == null ){
+                httpResponse.forwardWebResource(httpRequest.getPath());
+            } else {
+                controller.service(httpRequest, httpResponse);
+            }
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+
+}
