9fea5555fa7ecbdaa529633058a9ffaddf1299d9
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-07-31 12:07:08 +0900

    오류 수정

---
+++ src/main/java/com/spring/http/HttpRequestSupport.java
@@ -0,0 +1,12 @@
+package com.spring.http;
+
+interface HttpRequestSupport {
+
+    String getPath();
+    String getMethod();
+    String getVersion();
+    String getHeader(String key);
+    String getCookies(String key);
+    String getParameter(String key);
+    String getRequestBody();
+}
