99a45b47edaca41c3b592e5d0b1d5785f808876a
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-07-31 12:08:07 +0900

    HttpRuquest 테스트 코드 추가

---
+++ src/test/java/com/spring/http/HttpRequestTest.java
@@ -0,0 +1,37 @@
+package com.spring.http;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpRequestTest {
+
+    HttpRequest httpRequest;
+
+    @BeforeAll
+    void setUp(){
+    }
+
+    @Test
+    void getHeader() {
+    }
+
+    @Test
+    void getCookie() {
+    }
+
+    @Test
+    void getMethod() {
+    }
+
+    @Test
+    void getPath() {
+    }
+
+    @Test
+    void getVersion() {
+//        assertEquals("1.1"httpRequest.getVersion(), );
+
+    }
+}
