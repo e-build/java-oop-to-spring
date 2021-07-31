4b0ca37ae2eb9c38a92b258d41327edd17bfbbed
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-07-31 19:19:07 +0900

    Json 유틸 클래스 추가

--- src/main/java/com/framework/utils/IOUtils.java
+++ src/main/java/com/framework/utils/IOUtils.java
@@ -1,9 +1,10 @@
-package com.spring.utils;
+package com.framework.utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class IOUtils {
+
     public static String readData(BufferedReader br, int length) throws IOException {
         char[] body = new char[length];
         br.read(body, 0, length);
---
+++ src/main/java/com/framework/utils/JsonUtils.java
@@ -0,0 +1,30 @@
+package com.framework.utils;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtils {
+
+    private JsonUtils(){}
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static String serialize(Object obj) {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object deserialize(String jsonString, Class type) {
+        try {
+            return mapper.readValue(jsonString, type);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
