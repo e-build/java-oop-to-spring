05b6577bbfff6936e056c8ba3a5268ec1f8c2eed
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-05 16:35:55 +0900

    사용자 클래스 추가

---
+++ src/main/java/com/bussiness/user/User.java
@@ -0,0 +1,16 @@
+package com.bussiness.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String nickname;
+}
