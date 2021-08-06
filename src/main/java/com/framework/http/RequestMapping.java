c1708cea8e8beaa2f07e751dbdd18ced567f604a
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-06 14:02:41 +0900

    HTTP 요청 - 컨트롤러 매핑 클래스 추가

---
+++ src/main/java/com/framework/http/RequestMapping.java
@@ -0,0 +1,28 @@
+package com.framework.http;
+
+import com.bussiness.home.HomePageController;
+import com.bussiness.user.controller.UserListPageController;
+import com.bussiness.user.controller.UserLoginApiController;
+import com.bussiness.user.controller.UserLoginPageController;
+import com.framework.http.constants.HttpMethod;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+public class RequestMapping {
+
+    private static final Map<HandlerKey, Controller> controllers = Maps.newHashMap();
+
+    static {
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/"), new HomePageController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/user/login"), new UserLoginPageController());
+        controllers.put(HandlerKey.of(HttpMethod.POST, "/user/login"), new UserLoginApiController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/user/list"), new UserListPageController());
+    }
+
+    public static Controller getController(HandlerKey handlerKey) {
+        return controllers.get(handlerKey);
+    }
+
+
+}
