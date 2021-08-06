4edd16eedf5503f4f21f00e66ea8e7c497664942
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-06 14:02:20 +0900

    비즈니스 컨트롤러 생성

---
+++ src/main/java/com/bussiness/user/controller/UserLoginApiController.java
@@ -0,0 +1,13 @@
+package com.bussiness.user.controller;
+
+import com.framework.http.Controller;
+import com.framework.http.HttpRequest;
+import com.framework.http.HttpResponse;
+
+public class UserLoginApiController implements Controller {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+        
+    }
+}
