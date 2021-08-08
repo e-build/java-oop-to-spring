788c099f7936e5c43421bbb5e9d532f2ab1b4d38
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-09 08:33:07 +0900

    주석 추가

--- src/main/java/com/framework/web/server/RequestHandler.java
+++ src/main/java/com/framework/web/server/RequestHandler.java
@@ -3,11 +3,8 @@ package com.framework.web.server;
 import java.io.*;
 import java.net.Socket;
 
-import com.bussiness.Database;
-import com.bussiness.user.domain.User;
 import com.framework.http.*;
 import com.framework.utils.WebAppUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,18 +20,27 @@ public class RequestHandler extends Thread {
     public void run() {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
+            // 1. 요청, 응답 인스턴스 생성
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
-            response.addHeader("Content-Type", parseContentType(request.getHeader("Accept"))+";charset=utf-8");
             log.info("[REQUEST] {} {}", request.getMethod(), request.getUrl());
 
+            // 2. 공통으로 적용되는 응답 헤더 세팅
+            response.addHeader("Content-Type", parseContentType(request.getHeader("Accept"))+";charset=utf-8");
+
+            // 3. HTTP 요청에 해당하는 Controller 확인
             Controller controller = RequestMapping.getController(HandlerKey.of(request.getMethod(), request.getPath()));
 
+            // 4. HTTP 요청 처리
             if (controller != null){
+                // 4-1. Controller 로 작업 위임
                 controller.service(request, response);
             } else {
-                if ( existsResourceFile(request.getPath()) )
+                // 4-2. 정적자원 포워딩
+                if ( WebAppUtils.existsResourceFile(request.getPath()) )
                     response.forwardResource(request.getPath());
+                // 4-3. HTTP 요청에 해당하는 자원 X
                 else
                     response.responseBody("404");
             }
@@ -42,10 +48,6 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-    }
-
-    private boolean existsResourceFile(String resourceFilePath){
-        return new File(WebAppUtils.WEBAPP_ROOT_PATH + resourceFilePath).exists();
     }
 
     private String parseContentType(String accept){
