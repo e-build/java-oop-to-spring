552773925f472fb8467943b915bdb8a48b18a1b4
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-09 14:48:01 +0900

    RequestMapping Url-컨트롤러 매핑

--- src/main/java/com/framework/http/RequestMapping.java
+++ src/main/java/com/framework/http/RequestMapping.java
@@ -1,8 +1,7 @@
 package com.framework.http;
 
 import com.bussiness.home.HomePageController;
-import com.bussiness.user.controller.UserLoginApiController;
-import com.bussiness.user.controller.UserLoginPageController;
+import com.bussiness.user.controller.*;
 import com.framework.http.constants.HttpMethod;
 import com.google.common.collect.Maps;
 
@@ -13,13 +12,25 @@ public class RequestMapping {
     private static final Map<HandlerKey, Controller> controllers = Maps.newHashMap();
 
     static {
+        // PAGE
         controllers.put(HandlerKey.of(HttpMethod.GET, "/"), new HomePageController());
         controllers.put(HandlerKey.of(HttpMethod.GET, "/user/login"), new UserLoginPageController());
-        controllers.put(HandlerKey.of(HttpMethod.POST, "/user/login"), new UserLoginApiController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/user/register"), new UserRegistPageController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/recipe/list"), new MyMainPageController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/recipe/detail"), new MyMainPageController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/my"), new MyMainPageController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/my/info"), new MyInfoPageController());
+
+        // API
+        controllers.put(HandlerKey.of(HttpMethod.POST, "/api/user/register"), new UserRegistApiController());
+        controllers.put(HandlerKey.of(HttpMethod.POST, "/api/user/login"), new UserLoginApiController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/recipe/list"), new MyMainPageController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/recipe/detail"), new MyMainPageController());
+        controllers.put(HandlerKey.of(HttpMethod.POST, "/api/my/info"), new MyInfoUpdateApiController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/my/info"), new MyInfoReadApiController());
     }
 
     public static Controller getController(HandlerKey handlerKey) {
         return controllers.get(handlerKey);
     }
-
 }
