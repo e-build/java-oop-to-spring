a72296ecb0d51286f88744f06d98370c4de203c8
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-09 14:41:10 +0900

    레시피 도메인 클래스 추가

--- src/main/java/com/bussiness/recipe/domain/Recipe.java
+++ src/main/java/com/bussiness/recipe/domain/Recipe.java
@@ -1,4 +1,16 @@
 package com.bussiness.recipe.domain;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class Recipe {
+
+    private String name;
+    private String contents;
+    private Integer createdBy;
+    private LocalDateTime createdAt;
+    private Integer updateBy;
+    private LocalDateTime updateAt;
 }
