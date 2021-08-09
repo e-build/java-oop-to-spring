8e2ef377f4138e42a62d59135e3f5f821b969712
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-09 15:02:15 +0900

    수정된 API 설계에 따라 RequestMapping 수정

--- src/main/java/com/framework/http/RequestMapping.java
+++ src/main/java/com/framework/http/RequestMapping.java
@@ -1,6 +1,10 @@
 package com.framework.http;
 
 import com.bussiness.home.HomePageController;
+import com.bussiness.recipe.controller.RecipeDetailApiController;
+import com.bussiness.recipe.controller.RecipeDetailPageController;
+import com.bussiness.recipe.controller.RecipeListApiController;
+import com.bussiness.recipe.controller.RecipeListPageController;
 import com.bussiness.user.controller.*;
 import com.framework.http.constants.HttpMethod;
 import com.google.common.collect.Maps;
@@ -16,18 +20,15 @@ public class RequestMapping {
         controllers.put(HandlerKey.of(HttpMethod.GET, "/"), new HomePageController());
         controllers.put(HandlerKey.of(HttpMethod.GET, "/user/login"), new UserLoginPageController());
         controllers.put(HandlerKey.of(HttpMethod.GET, "/user/register"), new UserRegistPageController());
-        controllers.put(HandlerKey.of(HttpMethod.GET, "/recipe/list"), new MyMainPageController());
-        controllers.put(HandlerKey.of(HttpMethod.GET, "/recipe/detail"), new MyMainPageController());
-        controllers.put(HandlerKey.of(HttpMethod.GET, "/my"), new MyMainPageController());
-        controllers.put(HandlerKey.of(HttpMethod.GET, "/my/info"), new MyInfoPageController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/recipe/list"), new RecipeListPageController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/recipe/detail"), new RecipeDetailPageController());
 
         // API
         controllers.put(HandlerKey.of(HttpMethod.POST, "/api/user/register"), new UserRegistApiController());
         controllers.put(HandlerKey.of(HttpMethod.POST, "/api/user/login"), new UserLoginApiController());
-        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/recipe/list"), new MyMainPageController());
-        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/recipe/detail"), new MyMainPageController());
-        controllers.put(HandlerKey.of(HttpMethod.POST, "/api/my/info"), new MyInfoUpdateApiController());
-        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/my/info"), new MyInfoReadApiController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/user/logout"), new UserLogoutApiController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/recipe/list"), new RecipeListApiController());
+        controllers.put(HandlerKey.of(HttpMethod.GET, "/api/recipe/detail"), new RecipeDetailApiController());
     }
 
     public static Controller getController(HandlerKey handlerKey) {
