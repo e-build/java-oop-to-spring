d93e0e981c23061d8b0708230c7ea760acd523d7
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-09 19:49:40 +0900

    사용자 로그인, 회원가입 API 작성완료

--- src/main/java/com/bussiness/user/controller/UserLoginApiController.java
+++ src/main/java/com/bussiness/user/controller/UserLoginApiController.java
@@ -1,6 +1,7 @@
 package com.bussiness.user.controller;
 
 import com.bussiness.Database;
+import com.bussiness.user.domain.User;
 import com.framework.http.Controller;
 import com.framework.http.HttpRequest;
 import com.framework.http.HttpResponse;
@@ -13,7 +14,6 @@ public class UserLoginApiController implements Controller {
 
     @Override
     public void service(HttpRequest request, HttpResponse response) {
-
         Map<String, String> bodyParams = QueryStringUtils.toMap(request.getRequestBody());
         if ( login(bodyParams.get("username"), bodyParams.get("password")) ){
             response.addHeader("Set-Cookie", "login=true");
@@ -24,10 +24,9 @@ public class UserLoginApiController implements Controller {
     }
 
     private boolean login(String username, String password){
-        for ( Integer key : Database.USER.keySet() ){
-            if (StringUtils.equals(Database.USER.get(key).getUsername(), username))
-                return StringUtils.equals(Database.USER.get(key).getUsername(), password);
-        }
-        return false;
+        User user = Database.readUserByUsername(username);
+        if (user == null)
+            return false;
+        return StringUtils.equals(user.getPassword(), password);
     }
 }
