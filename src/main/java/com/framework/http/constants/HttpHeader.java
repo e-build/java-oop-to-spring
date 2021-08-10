6f4e42ed1deab5b1a39f430d6cffd4d1692e46db
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-10 09:36:51 +0900

    HttpHeader 상수관리용 Enum 클래스 추가

--- src/main/java/com/framework/http/constants/HttpHeader.java
+++ src/main/java/com/framework/http/constants/HttpHeader.java
@@ -1,4 +1,18 @@
 package com.framework.http.constants;
 
+import lombok.Getter;
+
 public enum HttpHeader {
+
+    CONTENT_TYPE("Content-Type"),
+    ACCEPT("Accept");
+
+    @Getter
+    private final String value;
+
+    HttpHeader(String value){
+        this.value = value;
+    }
+
+
 }
