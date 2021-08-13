c4b5b8684b3dde724c9b27517b72ac06cd30972f
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-14 00:09:25 +0900

    LocalDateTime => java.sql.Timestamp

--- src/main/java/com/bussiness/recipe/domain/Recipe.java
+++ src/main/java/com/bussiness/recipe/domain/Recipe.java
@@ -1,17 +1,21 @@
 package com.bussiness.recipe.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+@Setter
 @Getter
 public class Recipe {
 
+    private int id;
     private String name;
     private String contents;
     private String category;
-    private String createdBy;
-    private LocalDateTime createdAt;
-    private String updateBy;
-    private LocalDateTime updateAt;
+    private int createdBy;
+    private Timestamp createdAt;
+    private int updateBy;
+    private Timestamp updateAt;
 }
