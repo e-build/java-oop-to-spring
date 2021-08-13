7e562152b05e001e80232a7db459c3bca83f3f4e
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-13 21:21:59 +0900

    레시피 폼 페이지 html, controller 추가

---
+++ src/main/java/com/bussiness/recipe/controller/RecipeFormPageController.java
@@ -0,0 +1,11 @@
+package com.bussiness.recipe.controller;
+
+import com.framework.http.Controller;
+import com.framework.http.HttpRequest;
+import com.framework.http.HttpResponse;
+
+public class RecipeFormPageController implements Controller {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) { response.forward("/recipe/form"); }
+}
