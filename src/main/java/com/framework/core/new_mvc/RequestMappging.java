94ecea37e4600f092bbe272e5db045b23066b9f3
e-build <7311977-donggeol@users.noreply.gitlab.com>
2021-08-17 22:07:17 +0900

    Controller, RequestMappging 인터페이스 추가

---
+++ src/main/java/com/framework/core/new_mvc/RequestMappging.java
@@ -0,0 +1,16 @@
+package com.framework.core.new_mvc;
+
+import com.framework.http.constants.HttpMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestMappging {
+    String value() default "";
+
+    HttpMethod method() default HttpMethod.GET;
+}
