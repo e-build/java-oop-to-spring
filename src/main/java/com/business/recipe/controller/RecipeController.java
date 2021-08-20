c34e1077e74673afdcfc6b140ee7dc309d85c911
e-build <donggeol2@daum.net>
2021-08-20 23:13:26 +0900

    DI 프레임워크 개발에 필요한 코드 세팅 - Recipe : Controller - Service - Repository 생성자 의존관계 설정

--- src/main/java/com/business/recipe/controller/RecipeController.java
+++ src/main/java/com/business/recipe/controller/RecipeController.java
@@ -2,7 +2,9 @@ package com.business.recipe.controller;
 
 import com.business.recipe.dao.RecipeDao;
 import com.business.recipe.domain.Recipe;
+import com.business.recipe.service.RecipeService;
 import com.business.user.domain.User;
+import com.framework.core.di.annotation.Inject;
 import com.framework.core.new_mvc.annotation.Controller;
 import com.framework.core.new_mvc.annotation.RequestMapping;
 import com.framework.http.HttpRequest;
@@ -11,12 +13,24 @@ import com.framework.http.constants.HttpMethod;
 import com.framework.utils.DateUtils;
 import com.framework.utils.JsonUtils;
 import com.google.common.collect.Maps;
+import lombok.Getter;
 
 import java.util.Map;
 
 @Controller
 public class RecipeController {
+
     RecipeDao recipeDao = new RecipeDao();
+    RecipeService recipeService;
+
+    @Inject
+    public RecipeController(RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
+
+    public RecipeService getRecipeService(){
+        return recipeService;
+    }
 
     @RequestMapping(method = HttpMethod.GET, value = "/recipe/list")
     public void listPage(HttpRequest request, HttpResponse response){
@@ -62,4 +76,6 @@ public class RecipeController {
         bodyMap.put("result", recipeDao.insertOne(recipe));
         response.responseBody(bodyMap);
     }
+
+
 }
